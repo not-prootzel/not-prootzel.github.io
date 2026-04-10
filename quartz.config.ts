87d6ab80763b5c2312726efef748db35bced9cd3
Prootzel <lirkel123@gmail.com>
2026-04-10 13:16:15 +0200

    Quartz sync: Apr 10, 2026, 1:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -61,8 +61,8 @@ const config: QuartzConfig = {
       }),
       Plugin.SyntaxHighlighting({
         theme: {
-          light: "github-light",
-          dark: "github-dark",
+          light: "catppuccin-frappe",
+          dark: "catppuccin-frappe",
         },
         keepBackground: false,
       }),
